62ba2d77f3f7ba369980a85c70ab3da5f4a4e51e
SSOhana <v0uu0a@gmail.com>
2022-06-07 16:01:25 +0900

    add

--- app_home.py
+++ app_home.py
@@ -5,6 +5,9 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 import geopy.distance
 
+from streamlit_folium import st_folium
+import folium as g
+
 from PIL import Image 
 
 
@@ -13,9 +16,38 @@ def run_home() :
     img = Image.open('data/police_st.PNG')
     st.image(img)
    
+
+
+    #지도
+    police_loc = pd.read_csv('data/police_loc.csv')
+    pd.options.display.float_format = '{: .14f}'.format
+
+    # 위도
+    latitude = 37.4727937
+    # 경도
+    longitude = 126.6225932
+
+    m = g.Map(location=[latitude, longitude],
+               zoom_start=11)
+
+    police_loc.reset_index(inplace=True)
+    for i in range(len(police_loc)) :
+        marker01 = g.Marker([police_loc.loc[i]['위도'], police_loc.loc[i]['경도']],
+                            popup = police_loc.loc[i]['관서명'],
+                            icon=g.Icon('blue', icon='star')) 
+        marker01.add_to(m)
+
+    st_data = st_folium(m, widht = 725)
+
+
+
+
+
     st.subheader('위급상황을 위해 반드시 기억해야할 112 신고요령')
     video_file = open('data/emergency_112.mp4', 'rb')
     st.video(video_file)
+
+
 
 
 
