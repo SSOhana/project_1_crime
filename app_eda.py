743a128e7ef8dbdee3884f9260a1eda0a0bd9b9f
SSOhana <v0uu0a@gmail.com>
2022-06-08 11:13:14 +0900

    add

--- app_eda.py
+++ app_eda.py
@@ -19,14 +19,13 @@ import platform
 from matplotlib import font_manager, rc
 plt.rcParams['axes.unicode_minus'] = False
 
-if platform.system() == 'Darwin':
+if platform.system() == 'Linux':
     rc('font', family='AppleGothic')
 elif platform.system() == 'Windows':
     path = "c:/Windows/Fonts/malgun.ttf"
     font_name = font_manager.FontProperties(fname=path).get_name()
     rc('font', family=font_name)
 else:
-    matplotlib.rc('font', family='NanumGothic')
     print('Unknown system... sorry~~~~')
 
 
